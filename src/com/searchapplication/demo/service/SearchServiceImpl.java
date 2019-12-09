c6d4930e469fdcdaf5f27c8271ece3f0e29973d4
SagarikaPadhy <sagarika.padhy478@gmail.com>
2019-12-09 19:33:35 +1100

    code cleanup. log4j impl

--- src/com/searchapplication/demo/service/SearchServiceImpl.java
+++ src/com/searchapplication/demo/service/SearchServiceImpl.java
@@ -12,16 +12,35 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+/**
+ * Impl service for search application
+ *
+ * @author sagarika.padhy478@gmail.com
+ */
 
 @Service
 public class SearchServiceImpl implements SearchService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SearchServiceImpl.class);
 
+
+    /**
+     * get input from UI
+     *
+     * @param keyword , searchurl
+     * @return string searchResult. e.g "1,20" or "20"
+     */
     @Override
     public String getSearchResult(String keyword, String searchurl) {
 
         String searchResult = "";
         String res = "";
+
+        LOGGER.info("keyword  " + keyword + "search url " + searchurl);
+
         if (keyword.contains(",")) {
             String[] keywords = keyword.split(",");
             for (int i = 0; i < keywords.length; i++) {
@@ -29,15 +48,16 @@ public class SearchServiceImpl implements SearchService {
                 res = doSearchInGoogle(keywords[i], searchurl);
                 searchResult += "," + res;
             }
-            System.out.println("loop 1:" + searchResult);
+
 
         } else {
             searchResult = doSearchInGoogle(keyword, searchurl);
-            System.out.println("loop 1 1:" + searchResult);
+
         }
         return searchResult;
 
     }
+
 
     public String doSearchInGoogle(String keyword, String searchurl) {
 
@@ -45,16 +65,30 @@ public class SearchServiceImpl implements SearchService {
         List<String> result = new ArrayList<String>();
         String searchTerm = keyword.trim().replaceAll(" ", "+");
         String path = "https://www.google.com/search?q=" + searchTerm + "&num=100";
-        System.out.println("search url " + path);
+
+        LOGGER.info("Path " + searchurl);
+
         URL url;
         try {
+            /**
+             * perform search in google
+             * returns result in html stream.
+             */
             url = new URL(path);
             final URLConnection connection = url.openConnection();
             connection.setRequestProperty("User-Agent", agent);
             final InputStream stream = connection.getInputStream();
+
+            /**
+             * call getString() method
+             * to convert into a string
+             */
             String page = getString(stream);
 
-            //		System.out.println("google result "+ page);
+            /**
+             * call parseSearchResult() method
+             * parsing the html string using java regex
+             */
             result = parseSearchResult(page, searchurl);
 
         } catch (MalformedURLException e) {
@@ -67,11 +101,15 @@ public class SearchServiceImpl implements SearchService {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println("result 2:" + result.size());
+
         return String.valueOf(result.size());
 
     }
 
+
+    /**
+     * getString() method
+     */
     public static String getString(InputStream is) {
         StringBuilder sb = new StringBuilder();
 
@@ -97,6 +135,10 @@ public class SearchServiceImpl implements SearchService {
     }
 
 
+    /**
+     * parseSearchResult() method do to regex
+     */
+
     public static List<String> parseSearchResult(final String html, String searchrl) throws Exception {
         List<String> result = new ArrayList<String>();
         String pattern1 = "<a href=\"/url?q=";
@@ -106,10 +148,14 @@ public class SearchServiceImpl implements SearchService {
 
         while (m.find()) {
             String domainName = m.group(0);
+            LOGGER.info("after 1st regex " + domainName);
+
             Pattern urlPattern = Pattern.compile(Pattern.quote(searchrl));
             Matcher urlMatcher = urlPattern.matcher(domainName);
             while (urlMatcher.find()) {
                 String domain = urlMatcher.group(0);
+                LOGGER.info("after final regex " + domainName);
+
                 result.add(domain);
             }
 
