ada6ab65be5a096bb27db5790f8b4a4818d51c53
SagarikaPadhy <sagarika.padhy478@gmail.com>
2019-12-09 18:41:04 +1100

    working on regex

--- src/com/searchapplication/demo/service/SearchServiceImpl.java
+++ src/com/searchapplication/demo/service/SearchServiceImpl.java
@@ -17,98 +17,129 @@ import org.springframework.stereotype.Service;
 @Service
 public class SearchServiceImpl implements SearchService {
 
-	@Override
-	public String getSearchResult(String keyword, String searchrl) {
-		
-		String searchResult = "";
-		String res = "";
-		if(keyword.contains(",")) {
-			String[] keywords = keyword.split(",");			
-			for(int i=0; i<keywords.length; i++) {
-				
-				res = search(keywords[i],searchrl);
-				searchResult += ","+res;								 
-			}
-			System.out.println("loop 1:"+searchResult);
-			
-		}else {
-			searchResult = search(keyword,searchrl);
-			System.out.println("loop 1 1:"+searchResult);
-		}
-		return searchResult;
 
-	}
-	
-	public String search(String keyword, String searchrl) {
-		
-		final String agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
-		List<String> result = new ArrayList<String>();
-		String searchTerm = keyword.trim().replaceAll(" ", "+");
-		System.out.println("searchTerm 2"+searchTerm);
-		String path = "https://www.google.com/search?q=" + searchTerm + "&num=100";
-		URL url;
-		try {
-			url = new URL(path);
-			final URLConnection connection = url.openConnection();			                
-			connection.setRequestProperty("User-Agent", agent);
-			final InputStream stream = connection.getInputStream();
-			String page = getString(stream);
-			
-	//		System.out.println("google result "+ page);
-			result = parseLinks(page,searchrl);
-			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("result 2:"+result.size());
-		return String.valueOf(result.size());
-		
-	}
+    @Override
+    public String getSearchResult(String keyword, String searchrl) {
 
-	public static String getString(InputStream is) {
-		StringBuilder sb = new StringBuilder();
+        String searchResult = "";
+        String res = "";
+        if (keyword.contains(",")) {
+            String[] keywords = keyword.split(",");
+            for (int i = 0; i < keywords.length; i++) {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		String line;
-		try {
-			while ((line = br.readLine()) != null) {
-				sb.append(line);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			/** finally block to close the {@link BufferedReader} */
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return sb.toString();
-	}
+                res = search(keywords[i], searchrl);
+                searchResult += "," + res;
+            }
+            System.out.println("loop 1:" + searchResult);
 
-	public static List<String> parseLinks(final String html,String searchrl) throws Exception {
-		List<String> result = new ArrayList<String>();
-		String pattern1 = searchrl;  //"<h3 class=\"r\"><a href=\"/url?q="
-		//String pattern2 = "\">";
-		Pattern p = Pattern.compile(Pattern.quote(pattern1) );
-		Matcher m = p.matcher(html);
+        } else {
+            searchResult = search(keyword, searchrl);
+            System.out.println("loop 1 1:" + searchResult);
+        }
+        return searchResult;
 
-		while (m.find()) {
-			String domainName = m.group(0);
-			result.add(domainName);
+    }
 
-		}
-		return result;
-	}
+    public String search(String keyword, String searchrl) {
+
+        final String agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
+        List<String> result = new ArrayList<String>();
+        String searchTerm = keyword.trim().replaceAll(" ", "+");
+        System.out.println("searchTerm 2" + searchTerm);
+        String path = "https://www.google.com/search?q=" + searchTerm + "&num=100";
+        URL url;
+        try {
+            url = new URL(path);
+            final URLConnection connection = url.openConnection();
+            connection.setRequestProperty("User-Agent", agent);
+            final InputStream stream = connection.getInputStream();
+            String page = getString(stream);
+
+            //		System.out.println("google result "+ page);
+            result = parseLinks(page, searchrl);
+
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("result 2:" + result.size());
+        return String.valueOf(result.size());
+
+    }
+
+    /**
+     * Convert stream to string
+     *
+     * @param is
+     * @return string
+     */
+
+    public static String getString(InputStream is) {
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            /** finally block to close the {@link BufferedReader} */
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Parse the url using regex
+     * 1. Parse the tag first
+     * 2. Then parse the url from the tag
+     *
+     * @param html,searchUrl
+     * @return resukt arrayList
+     */
+
+    public static List<String> parseLinks(final String html, String searchUrl) throws Exception {
+        final String HTML_A_TAG_PATTERN = "(?i)<a([^>]+)>(.+?)</a>";
+        final String HTML_A_HREF_TAG_PATTERN =
+                "\\s*(?i)href\\s*=\\s*(\"([^\"]*\")|'[^']*'|([^'\">\\s]+))";
+        List<String> result = new ArrayList<String>();
+        Pattern patternTag, patternLink;
+        Matcher matcherTag, matcherLink;
+        patternTag = Pattern.compile(HTML_A_TAG_PATTERN);
+        patternLink = Pattern.compile(HTML_A_HREF_TAG_PATTERN);
+        matcherTag = patternTag.matcher(html);
+        while (matcherTag.find()) {
+
+            String href = matcherTag.group(1);
+            matcherLink = patternLink.matcher(href);
+
+            while (matcherLink.find()) {
+                String link = matcherTag.group(1);
+                Pattern patternURL = Pattern.compile(searchUrl);
+                Matcher matcherURL = patternURL.matcher(link);
+                while (matcherURL.find()) {
+                    String domainName = matcherURL.group(0);
+                    result.add(domainName);
+                }
+            }
+        }
+        return result;
+    }
 
 }
+
